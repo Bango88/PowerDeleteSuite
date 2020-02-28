e10b7aaba09b6c21885e7dbd5b03003b8d9e0332
Bango88 <47485594+Bango88@users.noreply.github.com>
2020-02-27 22:21:42 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,7 @@ javascript: (function() {
     var isReddit = document.location.hostname.split('.').slice(-2).join('.') === 'reddit.com';
     var isOverview = document.location.href.match(/\/overview\b/);
     if (isReddit && isOverview) {
-        var cachBustUrl = 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
+        var cachBustUrl = 'https://raw.githubusercontent.com/Bango88/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
         fetch(cachBustUrl).then(function(response) {
             return response.text();
         }).then(function(data) {
@@ -15,7 +15,7 @@ javascript: (function() {
             alert('Error retreiving PowerDeleteSuite from github');
         });
     } else if (confirm('This script can only be run from your own user profile on reddit. Would you like to go there now?')) {
-        document.location = 'https://old.reddit.com/u/me/overview';
+        document.location = 'https://reddit.com/u/me/overview';
     } else {
         alert('Please go to your reddit profile before running this script');
     }
